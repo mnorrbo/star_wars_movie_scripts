928cb2d9bb927545072f80c77d90ee47a913de05
Mandy Norrbo <mandy.norrbo@hotmail.com>
2021-02-16 17:03:27 +0000

    first commit

---
+++ cleaning.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(janitor)
+
+sw_4 <- read.table("raw_data/SW_EpisodeIV.txt") %>%
+  as_tibble() %>% 
+  mutate(episode = "4",
+         line = 1:nrow(.))
+sw_5 <- read.table("raw_data/SW_EpisodeV.txt") %>%
+  as_tibble() %>% 
+  mutate(episode = "5",
+         line = 1:nrow(.))
+sw_6 <- read.table("raw_data/SW_EpisodeVI.txt") %>%
+  as_tibble() %>% 
+  mutate(episode = "6",
+         line = 1:nrow(.))
+
+sw_trilogy <- sw_4 %>%
+  bind_rows(sw_5) %>% 
+  bind_rows(sw_6) %>%
+  mutate(character = str_to_title(character))
