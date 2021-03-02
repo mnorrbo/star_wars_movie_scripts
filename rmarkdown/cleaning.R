1705984aee58e2589c79019a15f2719393a8492a
Mandy Norrbo <mandy.norrbo@hotmail.com>
2021-03-02 18:11:38 +0000

    reshuffling, ipynb & rmd additions

--- rmarkdown/cleaning.R
+++ rmarkdown/cleaning.R
@@ -4,17 +4,17 @@ library(janitor)
 
 # Reading in data and labelling -------------------------------------------
 
-sw_4 <- read.table("raw_data/SW_EpisodeIV.txt") %>%
+sw_4 <- read.table("../raw_data/SW_EpisodeIV.txt") %>%
   as_tibble() %>% 
   mutate(episode = "4",
          line = 1:nrow(.))
 
-sw_5 <- read.table("raw_data/SW_EpisodeV.txt") %>%
+sw_5 <- read.table("../raw_data/SW_EpisodeV.txt") %>%
   as_tibble() %>% 
   mutate(episode = "5",
          line = 1:nrow(.))
 
-sw_6 <- read.table("raw_data/SW_EpisodeVI.txt") %>%
+sw_6 <- read.table("../raw_data/SW_EpisodeVI.txt") %>%
   as_tibble() %>% 
   mutate(episode = "6",
          line = 1:nrow(.))
@@ -30,4 +30,4 @@ sw_trilogy <- sw_4 %>%
 
 # Writing clean dataset ---------------------------------------------------
 
-write_csv(sw_trilogy, "clean_data/original_sw_trilogy.csv")
+write_csv(sw_trilogy, "../clean_data/original_sw_trilogy.csv")
